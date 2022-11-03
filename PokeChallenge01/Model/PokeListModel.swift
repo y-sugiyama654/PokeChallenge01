f95cc05c868e9e582616be4e36b9f16e74bc77c8
Tanaka Soushi <>
2022-11-04 00:46:40 +0900

    Add PokeListModel

---
+++ PokeChallenge01/Model/PokeListModel.swift
@@ -0,0 +1,15 @@
+//
+//  PokeListModel.swift
+//  PokeChallenge01
+//
+//  Created by Tanaka Soushi on 2022/11/04.
+//
+
+import Foundation
+
+final class PokeListModel {
+    private let data = ["Swift", "RxSwift", "MVVM", "Flux", "SwiftUI", "Flutter"]
+    func dummyData() -> [String] {
+        data
+    }
+}
