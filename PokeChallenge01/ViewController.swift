aeee1e1d045cc9d2c828a0efc1aef72a65de17db
Tanaka Soushi <>
2022-11-03 21:20:58 +0900

    first commit

---
+++ PokeChallenge01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeChallenge01
+//
+//  Created by Tanaka Soushi on 2022/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
