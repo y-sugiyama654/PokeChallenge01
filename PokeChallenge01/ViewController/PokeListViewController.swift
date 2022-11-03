6dcf47a3a389ebe5a24a8d0994d5d62a8daa0652
Tanaka Soushi <>
2022-11-03 22:15:08 +0900

    Fix class name `Main` to `PokeListViewController` / Add TableView in PokeListViewController.storyboard

--- PokeChallenge01/ViewController/PokeListViewController.swift
+++ PokeChallenge01/ViewController/PokeListViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet var tableview: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
