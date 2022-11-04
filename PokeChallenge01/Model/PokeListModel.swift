c1dde82628bb505f5f7a0eab952ef158bae72abf
Tanaka Soushi <>
2022-11-04 18:09:09 +0900

    Show fetchPokemonData result in PokeListViewController's tableview

--- PokeChallenge01/Model/PokeListModel.swift
+++ PokeChallenge01/Model/PokeListModel.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
+protocol PokeListModelDelegate: AnyObject {
+    func fetchPokemonDataSuccess()
+    func fetchPokemonDataFailure(error: PokeAPIError)
+}
+
 final class PokeListModel {
-    private let data = ["Swift", "RxSwift", "MVVM", "Flux", "SwiftUI", "Flutter"]
-    func dummyData() -> [String] {
-        data
+    private(set) var pokemons: [Pokemon] = []
+    weak var delegate: PokeListModelDelegate?
+    let repository = PokeRepository()
+    func fetchPokemonData(limit: Int = 100) {
+        repository.fetchPokemon(limit: limit, { [weak self] result in
+            switch result {
+            case let .success(success):
+                self?.pokemons = success.datas
+                self?.delegate?.fetchPokemonDataSuccess()
+            case let .failure(error):
+                self?.delegate?.fetchPokemonDataFailure(error: error as! PokeAPIError)
+            }
+        })
     }
 }
