a96bae29aeb986cee354a5108ed89cbfb63f3c4e
Yuta <y.sugiyama54@gmail.com>
2022-12-15 01:28:13 +0900

    画像データを取得し画像を表示

--- PokeChallenge01/View/PokeTableViewCell.swift
+++ PokeChallenge01/View/PokeTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class PokeTableViewCell: UITableViewCell {
     
+    let imageUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
     
     @IBOutlet private var numberLabel: UILabel!
     @IBOutlet private var titleLabel: UILabel!
@@ -17,9 +18,7 @@ final class PokeTableViewCell: UITableViewCell {
     @IBOutlet private var defenseLabel: UILabel!
     @IBOutlet private var specialDefenseLabel: UILabel!
     @IBOutlet private var speedLabel: UILabel!
-    
-    @IBOutlet private var pokeView: UIImageView!
-    
+    @IBOutlet private var pokemonImage: UIImageView!
     @IBOutlet private var specialAttackLabel: UILabel!
     
     func configure(pokemon: Pokemon) {
@@ -31,6 +30,18 @@ final class PokeTableViewCell: UITableViewCell {
         specialAttackLabel.text = "とくこう \(pokemon.status.specialAttack)"
         specialDefenseLabel.text = "とくぼう \(pokemon.status.specialDefense)"
         speedLabel.text = "すばやさ \(pokemon.status.speed)"
+        setImage(id: pokemon.id)
+    }
+    
+    private func setImage(id: Int) {
+        let request = URLRequest(url: URL(string: imageUrl + "\(id).png")!)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async { [weak self] in
+                self?.pokemonImage.image = UIImage(data: data)
+            }
+        }
+        .resume()
     }
     
 }
